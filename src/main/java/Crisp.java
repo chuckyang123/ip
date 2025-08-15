bf6b9bbbe93f99ffd2dcf516f306edf5742e14da
chuckyang123 <xiangwall0926@gmail.com>
2025-08-15 11:42:14 +0800

    Level 3 marking

--- src/main/java/Crisp.java
+++ src/main/java/Crisp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Crisp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("____________________________________________________________");
@@ -19,14 +19,33 @@ public class Crisp {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+
             } else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
+
+            } else if (input.startsWith("mark ")) {
+                int taskNum = Integer.parseInt(input.substring(5)) - 1;
+                tasks[taskNum].markDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[taskNum]);
+                System.out.println("____________________________________________________________");
+
+            } else if (input.startsWith("unmark ")) {
+                int taskNum = Integer.parseInt(input.substring(7)) - 1;
+                tasks[taskNum].markUndone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[taskNum]);
+                System.out.println("____________________________________________________________");
+
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
