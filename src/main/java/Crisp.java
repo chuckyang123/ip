b095688eb181798378b9933bc0976f84f13823e5
chuckyang123 <xiangwall0926@gmail.com>
2025-08-15 11:36:36 +0800

    Level 2 task

--- src/main/java/Crisp.java
+++ src/main/java/Crisp.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Crisp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Crisp");
@@ -17,11 +19,19 @@ public class Crisp {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
-
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
 
         sc.close();
